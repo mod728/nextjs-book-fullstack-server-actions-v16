3756f350de4340e1ee53a903a93323ba8b2b5b2e
mod728 <phallus-phaser@hotmail.com>
2025-11-06 10:16:45 +0100

    Code for Chapter 2

---
+++ app/actions/itemDelete.js
@@ -0,0 +1,12 @@
+"use server"
+import connectDB from "../utils/database"
+import { ItemModel } from "../utils/schemaModels"
+
+export const itemDelete = async(id) => {
+    try{
+        await connectDB() 
+        await ItemModel.deleteOne({_id: id})
+    }catch{
+        throw new Error("エラー：アイテム削除失敗")
+    }
+}
---
+++ app/item/update/[id]/page.js
@@ -0,0 +1,24 @@
+import { itemReadSingle } from "../../readsingle/[id]/page"
+import { itemUpdate } from "../../../actions/itemUpdate" 
+
+const UpdateItem = async(context) => {                
+    const singleItem = await itemReadSingle(context)  
+
+    const params = await context.params
+    const itemUpdateWithId = itemUpdate.bind(null, params.id) 
+
+    return (
+        <div>
+            <h1>アイテム編集</h1>
+            <form action={itemUpdateWithId}> 
+                <input defaultValue={singleItem.title} type="text" name="title" placeholder="アイテム名" required/>
+                <input defaultValue={singleItem.price} type="text" name="price" placeholder="価格" required/>
+                <input defaultValue={singleItem.image} type="text" name="image" placeholder="画像" required/>
+                <textarea defaultValue={singleItem.description} name="description" rows={15} placeholder="商品説明" required></textarea>
+                <button>編集</button>
+            </form>                        
+        </div>
+    )
+}
+
+export default UpdateItem
